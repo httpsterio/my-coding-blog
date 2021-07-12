7bd82faf99a2f8e6d8a16f04e3f9ea5576d601f7
Sami Singh <sami@httpster.io>
2021-07-12 19:12:17 +0300

    dang mate

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,9 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("./src/sass/");
   eleventyConfig.addPassthroughCopy("./src/assets");
-  module.exports = function(eleventyConfig) {
-    eleventyConfig.setBrowserSyncConfig({
+  eleventyConfig.setBrowserSyncConfig({
       files: './public/css/**/*.css'
     });
-  };
 
   return {
     dir: {
