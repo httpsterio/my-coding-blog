52b4d00677413372e2d971bc0af85b8139ca4602
Sami Singh <sami@httpster.io>
2021-07-12 19:05:30 +0300

    initial commit

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,11 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("./src/sass/");
   eleventyConfig.addPassthroughCopy("./src/assets");
+  module.exports = function(eleventyConfig) {
+    eleventyConfig.setBrowserSyncConfig({
+      files: './public/css/**/*.css'
+    });
+  };
 
   return {
     dir: {
