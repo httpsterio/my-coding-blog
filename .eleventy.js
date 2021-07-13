0f9cad82f7bce6d39839dbfaf9380c81d69b000c
AyoCodess <ayo.daniel.adesanya@gmail.com>
2021-07-13 16:01:48 +0200

    blog eror

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,15 @@
+const { DateTime } = require("luxon");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("./src/sass/");
   eleventyConfig.addPassthroughCopy("./src/assets");
   eleventyConfig.setBrowserSyncConfig({
-      files: './public/css/**/*.css'
-    });
+    files: "./public/css/**/*.css",
+  });
+
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+  });
 
   return {
     dir: {
